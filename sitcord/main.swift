82837089a0ef4b3779d5e16ecfe1c4e41d951e93
Kyle Brodie <kylecbrodie@gmail.com>
2020-08-28 10:50:03 -0700

    Initial Commit

---
+++ sitcord/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sitcord
+//
+//  Created by Kyle Brodie on 2020-08-28.
+//  Copyright © 2020 Reveel, LLC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

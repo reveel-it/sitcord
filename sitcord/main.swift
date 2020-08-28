e574ea88f51e8faa0c0a1d032a5c713754713147
Kyle Brodie <kylecbrodie@gmail.com>
2020-08-28 12:16:06 -0700

    Hook in to macOS sleep/wake lock/unlock events to trigger automation of discord

--- sitcord/main.swift
+++ sitcord/main.swift
@@ -2,11 +2,77 @@
 //  main.swift
 //  sitcord
 //
-//  Created by Kyle Brodie on 2020-08-28.
+//  Created by Kyle Brodie, Jonathan Ming on 2020-08-28.
 //  Copyright © 2020 Reveel, LLC. All rights reserved.
 //
-
 import Foundation
+import AppKit
 
-print("Hello, World!")
+func automateDiscord(sit: Bool) -> Bool {
+    print("Telling automateDiscord to", sit ? "sit" : "stand")
+    let task = Process()
+    let stdout = Pipe()
+    task.standardOutput = stdout;
+    task.executableURL = URL.init(fileURLWithPath: "/usr/bin/env", isDirectory: false)
+    task.arguments = ["node", "./automateDiscord.js", "--", sit ? "--sit" : "--stand"]
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus == 0
+}
 
+class SitcordObserver {
+    var state: String
+
+    init() {
+        state = "awake"
+    }
+
+    @objc
+    func sleepFn() {
+        state = "asleep"
+        let result = automateDiscord(sit: false)
+        print(result)
+    }
+
+    @objc
+    func wakeFn() {
+        state = "awake"
+        let result = automateDiscord(sit: true)
+        print(result)
+    }
+
+    @objc
+    func status() {
+        print(state)
+    }
+}
+
+func main() {
+    let obs = SitcordObserver()
+
+    let workspaceNotifCenter = NSWorkspace.shared.notificationCenter;
+    workspaceNotifCenter.addObserver(obs, selector: #selector(SitcordObserver.sleepFn), name: NSWorkspace.willSleepNotification, object: nil)
+    workspaceNotifCenter.addObserver(obs, selector: #selector(SitcordObserver.wakeFn), name: NSWorkspace.didWakeNotification, object: nil)
+
+    let distribNotifCenter = DistributedNotificationCenter.default;
+    distribNotifCenter.addObserver(obs, selector: #selector(SitcordObserver.sleepFn), name: NSNotification.Name("com.apple.screenIsLocked"), object: nil)
+    distribNotifCenter.addObserver(obs, selector: #selector(SitcordObserver.wakeFn), name: NSNotification.Name("com.apple.screenIsUnlocked"), object: nil)
+    
+    // print status to keep app alive
+    Timer.scheduledTimer(timeInterval: 60, target: obs, selector: #selector(SitcordObserver.status), userInfo: nil, repeats: true)
+    RunLoop.current.run()
+}
+
+public func registerSigint() -> DispatchSourceSignal {
+    signal(SIGINT, SIG_IGN)
+    let sigintSrc = DispatchSource.makeSignalSource(signal: SIGINT, queue: .main)
+    sigintSrc.setEventHandler {
+        print("Got SIGINT")
+        exit(0)
+    }
+    sigintSrc.resume()
+    return sigintSrc
+}
+
+let source = registerSigint()
+main()

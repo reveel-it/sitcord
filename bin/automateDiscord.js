b8a3529361cfc609c86a24a6a34be92019040fcc
Jonathan Ming <jming422@gmail.com>
2021-11-12 13:35:00 -0800

    Update to be swiftier

--- bin/automateDiscord.js
+++ bin/automateDiscord.js
@@ -3,30 +3,20 @@ const puppeteer = require("puppeteer-core");
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
-const DISCORD_DEBUG_PORT = process.env.DISCORD_DEBUG_PORT;
-
-const DISCORD_CHANNEL_NAME = process.env.DISCORD_CHANNEL_NAME || "General";
-const DISCORD_SERVER_NAME = process.env.DISCORD_SERVER_NAME;
-
-const serverXPath = `//div[contains(@aria-label, "Servers")]//div[@role="treeitem" and contains(@aria-label, "${DISCORD_SERVER_NAME}")]`;
-
-const connectXPath = `//*[@role="button" and contains(@aria-label, "${DISCORD_CHANNEL_NAME} (voice channel)")]`;
-const disconnectXPath = '//button[@aria-label="Disconnect"]';
-
-async function getWSEndpoint() {
-  if (!DISCORD_DEBUG_PORT) {
+async function getWSEndpoint(port) {
+  if (!port) {
     throw Error(
-      `Environment variable DISCORD_DEBUG_PORT not found - you won't be able to connect to the Discord app without this!`
+      `No debugging port specified - you won't be able to connect to the Discord app without this!`
     );
   }
-  const { data } = await axios.get(
-    `http://localhost:${DISCORD_DEBUG_PORT}/json/version`
-  );
+  const { data } = await axios.get(`http://localhost:${port}/json/version`);
   return data.webSocketDebuggerUrl;
 }
 
-async function doInDiscord(fn) {
-  const browserWSEndpoint = await getWSEndpoint();
+async function doInDiscord(port, server, fn) {
+  const serverXPath = `//div[contains(@aria-label, "Servers")]//div[@role="treeitem" and contains(@aria-label, "${server}")]`;
+
+  const browserWSEndpoint = await getWSEndpoint(port);
   const browser = await puppeteer.connect({
     browserWSEndpoint,
     defaultViewport: null,
@@ -44,8 +34,9 @@ async function doInDiscord(fn) {
   }
 }
 
-async function sit() {
-  await doInDiscord(async (page) => {
+async function sit(port, server, channel) {
+  const connectXPath = `//*[@role="button" and contains(@aria-label, "${channel} (voice channel)")]`;
+  await doInDiscord(port, server, async (page) => {
     const connectBtn = await page.waitForXPath(connectXPath, {
       timeout: 3000,
     });
@@ -56,8 +47,9 @@ async function sit() {
   });
 }
 
-async function stand() {
-  await doInDiscord(async (page) => {
+async function stand(port, server) {
+  const disconnectXPath = '//button[@aria-label="Disconnect"]';
+  await doInDiscord(port, server, async (page) => {
     try {
       const disconnectBtn = await page.waitForXPath(disconnectXPath, {
         timeout: 3000,
@@ -75,6 +67,8 @@ async function stand() {
 async function main() {
   const args = yargs(hideBin(process.argv)).argv;
 
+  const { port, server, channel } = args;
+
   if (!!args.sit && !!args.stand) {
     throw Error(
       "Don't specify both --sit and --stand at the same time ya dingus!"
@@ -85,10 +79,10 @@ async function main() {
   }
 
   if (!!args.sit) {
-    await sit();
+    await sit(port, server, channel || "General");
     console.log("sat");
   } else {
-    await stand();
+    await stand(port, server);
     console.log("stood");
   }
 }
--- sitcord/main.swift
+++ sitcord/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  sitcord
 //
-//  Created by Kyle Brodie, Jonathan Ming on 2020-08-28.
-//  Copyright © 2020 Reveel, LLC. All rights reserved.
+//  Created by Kyle Brodie, Arte Ebrahimi, Jonathan Ming
+//  Copyright © 2021 Reveel, LLC. All rights reserved.
 //
 import Foundation
 import AppKit
@@ -12,8 +12,8 @@ import Darwin
 setbuf(__stdoutp, nil);
 setbuf(__stderrp, nil);
 
-func automateDiscord(sit: Bool) -> Bool {
-    print(NSDate(), sit ? "sit" : "stand", "-> automateDiscord.js")
+func automateDiscord(server: String, sit: Bool) -> Bool {
+    print(NSDate(), sit ? "sit" : "stand", "-> automateDiscord")
 
     let bundleLocation = Bundle.main.resourceURL?.standardizedFileURL ?? URL.init(fileURLWithPath: ".", isDirectory: true).standardizedFileURL
     let jsLocation = bundleLocation.appendingPathComponent("automateDiscord.js")
@@ -24,7 +24,7 @@ func automateDiscord(sit: Bool) -> Bool {
     task.standardOutput = stdoutP;
     task.standardError = stderrP;
     task.executableURL = URL.init(fileURLWithPath: "/usr/bin/env", isDirectory: false)
-    task.arguments = ["node", jsLocation.path, sit ? "--sit" : "--stand"]
+    task.arguments = ["node", jsLocation.path, sit ? "--sit" : "--stand", "--port=54321", "--server='\(server)'"]
     task.launch()
     task.waitUntilExit()
 
@@ -46,9 +46,11 @@ func automateDiscord(sit: Bool) -> Bool {
 
 class SitcordObserver {
     var sit: Bool
+    var server: String
 
-    init() {
+    init(iServer: String) {
         sit = true
+        server = iServer
     }
 
     func sit(n: Notification) {
@@ -76,40 +78,44 @@ class SitcordObserver {
     }
 }
 
-func exitIfDiscordExited(pid: String) {
-    let task = Process()
-    let stderrP = Pipe()
-    let stdoutP = Pipe()
-    task.standardError = stderrP;
-    task.standardOutput = stdoutP;
-    task.executableURL = URL.init(fileURLWithPath: "/bin/ps", isDirectory: false)
-    task.arguments = ["-p", pid]
-    task.launch()
-    task.waitUntilExit()
-    if task.terminationStatus != 0 {
-        print(NSDate(), "Discord no longer exists. Stopping...")
-        exit(0)
+class DiscordTerminatedObserver: NSObject {
+    @objc var objectToObserve: NSRunningApplication
+    var observation: NSKeyValueObservation?
+
+    init(object: NSRunningApplication) {
+        objectToObserve = object
+        super.init()
+
+        observation = observe(
+            \.objectToObserve.isTerminated,
+            options: [.initial, .new]
+        ) { object, change in
+            print(NSDate(), "Discord isTerminated changed to: \(change.newValue!)")
+            if change.newValue == true {
+                print(NSDate(), "Discord terminated. Stopping...")
+                exit(0)
+            }
+        }
     }
 }
 
 func main() {
     print(NSDate(), "Starting...")
-    let obs = SitcordObserver()
+
+    guard let discordUrl = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.hnc.Discord") else {
+        print(NSDate(), "Can't locate Discord app; is it installed?")
+        return
+    }
+
+    let discord = NSWorkspace.shared.openApplication(at: discordUrl, configuration: NSWorkspace.OpenConfiguration(arguments: ["--remote-debugging-port=54321"]))
+    let discordTermObs = DiscordTerminatedObserver(object: discord)
+
+    let obs = SitcordObserver("Focus Dev")
     let distribNotifCenter = DistributedNotificationCenter.default()
 
     distribNotifCenter.addObserver(forName: NSNotification.Name("com.apple.screenIsUnlocked"), object: nil, queue: nil, using: obs.sit)
     distribNotifCenter.addObserver(forName: NSNotification.Name("com.apple.screenIsLocked"), object: nil, queue: nil, using: obs.stand)
     
-    if CommandLine.arguments.count < 2 {
-        print(NSDate(), "Sitcord will not exit when Discord exits because Discord's PID wasn't provided as the first command line argument")
-    } else {
-        let pid = CommandLine.arguments[1]
-        print(NSDate(), "Discord PID:", pid)
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: {
-            (t: Timer) -> Void in exitIfDiscordExited(pid: pid)
-        })
-    }
-    Timer.scheduledTimer(withTimeInterval: 60, repeats: true, block: obs.status)
     RunLoop.current.run()
 }
 
